29d1922f03b5f16d67da24b0ce256b0507b9e5c2
voland2610 <vvoland2610@gmail.com>
2026-03-31 22:10:20 +0300

    feat: add styles welcomePage

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,15 +1,20 @@
 import { useNavigate } from "react-router";
+import styles from '../styles/welcomPage/WelcomPage.module.css'; // если styles на уровень выше
 
-function WelcomePage(){
-    const navigate = useNavigate();
-    const goToSettings = () => {
-    navigate("/ContentPage"); // меняет URL и рендерит компонент
+
+function WelcomePage() {
+  const navigate = useNavigate();
+  const goToSettings = () => {
+    navigate("/ContentPage"); 
   };
-    return(
-        <>
-            <button onClick={goToSettings}>asdasdasd</button>
-        </>
-    )
+  return (
+    <>
+      <div className={styles.button_center}>
+        <button className={styles.button} onClick={goToSettings}>Начать</button>
+      </div>
+      
+    </>
+  );
 }
 
 export default WelcomePage;
