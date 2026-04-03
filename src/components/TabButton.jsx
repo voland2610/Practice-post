010b537b64f178c5177e8660dd33ec4d3e01c992
voland2610 <vvoland2610@gmail.com>
2026-04-03 20:58:25 +0300

    feat: add click styles for tabs button

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -3,10 +3,17 @@ import { useNavigate } from "react-router";
 const TabButton = (props) => {
   const { label, path, styles } = props;
   const navigate = useNavigate();
-  const goToSettings = () => {
+  const goToSettings = (event) => {
     navigate(path);
+
+    const button = event.currentTarget;
+    
+    const allButtons = button.parentElement.querySelectorAll('button');
+    allButtons.forEach(btn => btn.classList.remove(styles['tab-color']));
+    
+    button.classList.add(styles['tab-color']);
+
   }
-  //сделать отображать куда нажал
   return (
     <>
       <button className={styles.tab} onClick={goToSettings}>{label}</button>
